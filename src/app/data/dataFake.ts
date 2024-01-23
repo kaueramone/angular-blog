33d7ea9b76d92e7f7a8f241dd075491cc3fd270e
Kaue Ramone <kaueramone@live.com>
2024-01-23 13:58:20 +0000

    content created

---
+++ src/app/data/dataFake.ts
@@ -0,0 +1,26 @@
+export const dataFake = [
+    {
+        "id": "1",
+        "photo": "https://www.adrenaline.com.br/wp-content/plugins/seox-image-magick/imagick_convert.php?width=910&height=568&format=.jpg&quality=91&imagick=uploads.adrenaline.com.br/2023/11/Omni-Man-ganha-trailer-de-gameplay-oficial-e-data-para-chegar-a-Mortal-Kombat-1-912x569.jpg",
+        "title": "Pro Kompetition de Mortal Kombat 1 chega ao Brasil",
+        "description": "Torneio Pro Kompetition de Mortal Kombat 1 vai acontecer no Brasil nos dias 2 e 3 de março, em São Paulo, oferecendo pontos na colocação global"
+    },
+    {
+        "id": "2",
+        "photo": "https://www.adrenaline.com.br/wp-content/plugins/seox-image-magick/imagick_convert.php?width=910&height=568&format=.jpg&quality=91&imagick=uploads.adrenaline.com.br/2023/08/lords-of-the-fallen-capa-912x569.jpg",
+        "title": "Ofertas da semana na Steam tem Lords of the Fallen e mais",
+        "description": "Lords of the Fallen, Conan Exiles, Valheim e Sea of Thieves são algumas das ofertas da semana da Steam"
+    },
+    {
+        "id": "3",
+        "photo": "https://www.adrenaline.com.br/wp-content/plugins/seox-image-magick/imagick_convert.php?width=910&height=568&format=.jpg&quality=91&imagick=uploads.adrenaline.com.br/2024/01/suicide-squad-joker-2-912x569.jpg",
+        "title": "Suicide Squad revela Joker jogável e roadmap inicial",
+        "description": "Confira como será jogar como Coringa, personagem que chega a Suicide Squad: Kill the Justice League em março"
+    },
+    {
+        "id": "4",
+        "photo": "https://www.adrenaline.com.br/wp-content/plugins/seox-image-magick/imagick_convert.php?width=910&height=568&format=.jpg&quality=91&imagick=uploads.adrenaline.com.br/2023/12/Cyberpunk-2077-vai-ganhar-patch-2.1-com-novos-elementos-de-gameplay-912x569.jpg",
+        "title": "Continuação de Cyberpunk 2077 pode ter elementos multiplayer [RUMOR]",
+        "description": "De acordo com CEO da CD Projekt Red, equipe de desenvolvimento estuda a inclusão de elementos multiplayer na continuação de Cyberpunk 2077"
+    }
+]
